9b06fd030ee597b0ee8e40915411371e2a4cbda3
NatsukiKomatsu <75403521+NatsukiKomatsu@users.noreply.github.com>
2020-12-27 17:18:58 +0900

    Update main1.js

--- main1.js
+++ main1.js
@@ -7,7 +7,7 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-let storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
+let storyText = 'It was 華氏94度 outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300ポンド, and it was a hot day.';
 let insertX = ['Willy the Goblin','Big Daddy','Father Christmas'];
 let insertY = ['the soup kitchen','Disneyland','the White House'];
 let insertZ = ['spontaneously combusted','melted into a puddle on the sidewalk','turned into a slug and crawled away'];
@@ -33,9 +33,9 @@ function result() {
 
   if(document.getElementById("jp").checked) {
     const weight = Math.round(300*0.0714286) + 'kg';
-    const temperature =  '摂氏' + Math.round((94-32) * 5 / 9) + '度' ;
-    newStory = newStory.replace('94 fahrenheit',temperature);
-    newStory = newStory.replace('300 pounds',weight);
+    const temperature =  '摂氏' + Math.round((94-32) * 5 / 9) ;
+    newStory = newStory.replace('華氏94',temperature);
+    newStory = newStory.replace('300ポンド',weight);
   }
 
   story.textContent = newStory;

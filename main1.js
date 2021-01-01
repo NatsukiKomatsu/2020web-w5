31110345696f4ddeba0aed3d053e68e4e8b4b91d
NatsukiKomatsu <75403521+NatsukiKomatsu@users.noreply.github.com>
2021-01-01 23:21:15 +0900

    Update main1.js

--- main1.js
+++ main1.js
@@ -7,10 +7,10 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-let storyText = '外は華氏94度で暑かったが、:insertx:は散歩に行きました。:insertx:は:inserty:。しばらく歩いていると:insertz:. Bobはそれを見て驚いたが,体重300ポンドの:insertx:は驚かなかった。なぜなら:inserty:では当たり前のことであるからだ。';
-let insertX = ['リラックマ','Big Daddy','Father Christmas'];
-let insertY = ['ハチミツの森に着きました','Disneyland','the White House'];
-let insertZ = ['ふさふさの胸毛が生えた茶色いくまがいた','melted into a puddle on the sidewalk','turned into a slug and crawled away'];
+let storyText = '外は華氏99度で暑かったが、:insertx:は散歩に行きました。:insertx:は:inserty:に着きました。しばらく歩いていると:insertz:. Bobはそれを見て驚いたが,体重560ポンドの:insertx:は驚かなかった。なぜなら:inserty:では当たり前のことであるからだ。';
+let insertX = ['リラックマ','かぐや姫','プーさん'];
+let insertY = ['ハチミツの森','竹林','ディズニーランド'];
+let insertZ = ['ふさふさの胸毛が生えた茶色いくまがいた','光り輝く竹があった','よく喋るたくさんの種類の動物がいた'];
 
 randomize.addEventListener('click', result);
 
@@ -34,10 +34,10 @@ function result() {
   }
 
   if(document.getElementById("jp").checked) {
-    const weight = Math.round(300*0.0714286) + 'kg';
-    const temperature =  '摂氏' + Math.round((94-32) * 5 / 9) ;
-    newStory = newStory.replace('華氏94',temperature);
-    newStory = newStory.replace('300ポンド',weight);
+    const weight = Math.round(560*0.0714286) + 'kg';
+    const temperature =  '摂氏' + Math.round((99-32) * 5 / 9) ;
+    newStory = newStory.replace('華氏99',temperature);
+    newStory = newStory.replace('560ポンド',weight);
   }
 
   story.textContent = newStory;
